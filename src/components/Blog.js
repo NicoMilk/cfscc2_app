0904acf8cd9415705002664530bb6e896ced171c
Nicolas Coulot <nicolas.coulot@epitech.eu>
2021-01-06 17:12:40 +0100

    WIP restart computer

---
+++ src/components/Blog.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import BlogpostApi from '../apis/Blogpost';
+
+class Blog extends React.Component {
+  render() {
+    // return <h1>CFSCC</h1>;
+    return (
+      <div>
+        <h1>Blog page</h1>
+      </div>
+    );
+  }
+}
+
+export default Blog;
